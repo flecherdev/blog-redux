3e2b40f34193eab794928fd427f09829c45b7927
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-09-14 10:12:21 -0300

    add reducer

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
+// redux
+import { createStore  } from 'redux';
+import { Provider } from 'react-redux'; 
+import reducers from './reducers';
+
+// create store to app
+const store = createStore(
+  reducers, // all reducers
+  {}, // initial state
+);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={ store }>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
