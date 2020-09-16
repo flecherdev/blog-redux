748bad618c7d627c86a43976d5b4984c68d66bd7
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-09-16 11:36:42 -0300

    try catch in users actions

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -2,10 +2,14 @@ import axios from 'axios';
 import { GET_USRS } from '../types/usersTypes';
 
 export const getUsers = () => async (dispach) => {
-    const resp = await axios.get('https://jsonplaceholder.typicode.com/users')
-
-    dispach({
-        type: GET_USRS,
-        payload: resp.data
-    });
+    try {
+        const resp = await axios.get('https://jsonplaceholder.typicode.com/users')
+    
+        dispach({
+            type: GET_USRS,
+            payload: resp.data
+        });
+    } catch (error) {
+        console.log(`Error: ${error.message}`)
+    }
 }
