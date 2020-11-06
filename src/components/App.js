e637da7b80f3fabb85380cc9765d936f24f87627
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-11-06 10:38:38 -0300

    added component publications

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Nav from './Nav';
 import User from './User';
+import Publications from './Publications'
 
 const Task = () => <div>Task</div> 
 
@@ -11,6 +12,7 @@ const App = () => (
     <div className='margen'>
       <Route exact path='/' component={ User }/> 
       <Route exact path='/task' component={ Task }/> 
+      <Route exact path='/publications/:key' component={ Publications }/> 
     </div>
   </BrowserRouter>
 )
