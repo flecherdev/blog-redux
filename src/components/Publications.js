ed133a6b6bcc3787eb83184be17fe2cbead2f569
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-11-09 22:53:08 -0300

    added screen publications and shared reducers

--- src/components/Publications.js
+++ src/components/Publications.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class Publications extends Component {
     render() {
+        console.log(this.props)
         return (
             <div>
+                <h1>Publications of </h1>
                 { this.props.match.params.key }
             </div>
         );
     }
 }
 
-export default Publications;
+const mapStateToProps = (reduces) => {
+    return reduces.usersReducer;
+}
+
+export default connect(mapStateToProps)(Publications);
